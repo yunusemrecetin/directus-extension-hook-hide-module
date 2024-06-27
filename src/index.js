3625c6f383ea08b8093a8dfc7c7f26fe4cf235c3
1Luc1 <lukas.aigner@gmail.com>
2024-06-27 09:44:44 +0200

    allow moderators to view modules

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
-export default ({ filter, action }) => {
+export default ({ filter, action }, { services }) => {
+  const { RolesService } = services;
+
   filter("settings.read", async (items, meta, context) => {
-    if (context.accountability && !context.accountability.admin) {
-      // Only display the defined modules for non-admin roles
+    const rolesService = new RolesService({
+      schema: context.schema,
+      accountability: context.accountability,
+    });
+    const moderatorRole = await rolesService.getKeysByQuery({
+      filter: { name: { _eq: "Moderator" } },
+    });
+
+    if (
+      context.accountability &&
+      !context.accountability.admin &&
+      !moderatorRole.includes(context.accountability.role)
+    ) {
+      // Only display the defined modules for non-admin & non-moderator roles
       var settings = items[0];
       settings.module_bar = [
         { type: "module", id: "landing-page", enabled: true },
