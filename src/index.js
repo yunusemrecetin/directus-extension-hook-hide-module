34ddbad45635a6f18bf5f0a73fa30817b592a770
Yunus Emre Cetin <yunusemrecetin@Yunuss-MacBook-Pro.local>
2024-08-02 02:34:46 +0300

    test

--- src/index.js
+++ src/index.js
@@ -18,9 +18,8 @@ export default ({ filter, action }, { services }) => {
       // Only display the defined modules for non-admin & non-moderator roles
       var settings = items[0];
       settings.module_bar = [
-        { type: "module", id: "Companies", enabled: false },
         { type: "module", id: "content", enabled: false },
-        { type: "module", id: "entries", enabled: true },
+        { type: "module", id: "companies", enabled: false },
         { type: "module", id: "files", enabled: true },
       ];
     }
