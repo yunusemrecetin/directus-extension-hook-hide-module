d00f5d01ee1e67fe7990b6d8ffb8a0445289e60e
Yunus Emre Cetin <yunusemrecetin@Yunuss-MacBook-Pro.local>
2024-08-02 02:23:28 +0300

    test

--- dist/index.js
+++ dist/index.js
@@ -1,17 +1,2 @@
 export default ({ filter, action }, { services }) => {
-  const { RolesService } = services;
-
-  filter("settings.read", async (items, meta, context) => {
-    const rolesService = new RolesService({
-      schema: context.schema,
-      accountability: context.accountability,
-    });
-    const moderatorRole = await rolesService.getKeysByQuery({
-      filter: { name: { _eq: "Satış Noktası yöneticisi" } },
-    });
-
-    console.log(moderatorRole.includes(context.accountability.role));
-    
-    return items;
-  });
 };
