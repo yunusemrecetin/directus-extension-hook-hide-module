7bd0f77a48c6d3819939600054800ce7b37a819a
Yunus Emre Cetin <yunusemrecetin@Yunuss-MacBook-Pro.local>
2024-08-02 03:16:08 +0300

    test

--- src/index.js
+++ src/index.js
@@ -10,21 +10,15 @@ export default ({ filter, action }, { services }) => {
       filter: { name: { _eq: "Satış Noktası yöneticisi" } },
     });
 
-    if (
-      context.accountability &&
-      !context.accountability.admin &&
-      moderatorRole.includes(context.accountability.role)
-    ) {
-      // Only display the defined modules for non-admin & non-moderator roles
-      console.dir(items);
-      var settings = items[0];
-      console.dir(settings);
-      settings.custom_css =
-        ".content-navigation > li:nth-child(1) { display: none !important;}";
-      console.dir(items[0]);
+    // Only display the defined modules for non-admin & non-moderator roles
+    console.dir(items);
+    var settings = items[0];
+    console.dir(settings);
+    settings.custom_css =
+      ".content-navigation > li:nth-child(1) { display: none !important;}";
+    console.dir(items[0]);
 
-      console.dir(items.length);
-    }
+    console.dir(items.length);
     return items;
   });
 };
