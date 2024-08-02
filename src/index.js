97028177e2eeca2b14a38bcada4b3f5d9ded8e60
Yunus Emre Cetin <yunusemrecetin@Yunuss-MacBook-Pro.local>
2024-08-02 03:33:25 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 export default ({ filter, action }, { services }) => {
+  const { RolesService } = services;
+
   filter("settings.read", async (items, meta, context) => {
-    console.log(`user status : ${!context.accountability.admin}`);
-    if (context.accountability && !context.accountability.admin) {
+    const rolesService = new RolesService({
+      schema: context.schema,
+      accountability: context.accountability,
+    });
+    const moderatorRole = await rolesService.getKeysByQuery({
+      filter: { name: { _eq: "Satış Noktası yöneticisi" } },
+    });
+
+    var checkPermission =
+      context.accountability &&
+      moderatorRole.includes(context.accountability.role);
+
+    console.dir(`check permission : ${checkPermission}`);
+
+    if (checkPermission) {
       // Only display the defined modules for non-admin & non-moderator roles
-      console.dir(items);
       var settings = items[0];
-      console.dir(settings);
+
       settings.custom_css =
         ".content-navigation > li:nth-child(1) { display: none !important;}";
-      console.dir(items[0]);
-
-      console.dir(items.length);
     }
+
+    console.dir(items);
+
     return items;
   });
 };
