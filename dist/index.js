bdfc7766ae9b68721cbbb1a98a211f2da11a9bbe
Yunus Emre Cetin <yunusemrecetin@Yunuss-MacBook-Pro.local>
2024-08-02 03:04:40 +0300

    test

--- dist/index.js
+++ dist/index.js
@@ -20,7 +20,7 @@ export default ({ filter, action }, { services }) => {
       var settings = items[0];
       console.dir(settings);
       settings.custom_css =
-        "#navigation > div.resize-wrapper.transition > div.module-nav.alt-colors > div.module-nav-content > div > ul > li:nth-child(1) { display: none !important;}";
+        '#navigation > div.resize-wrapper.transition > div.module-nav.alt-colors > div.module-nav-content > div > ul > li:nth-child(1) { display: none !important;}';
       console.dir(items[0]);
 
       console.dir(items.length);
